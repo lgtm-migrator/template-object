ec3940b9ee61fe431d1ce05352ec0b78c9c4f106
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-06-07 23:13:07 +0300

    Create displayBody.js

---
+++ displayBody.js
@@ -0,0 +1,36 @@
+import EmailTemplateBodyComponent from '../components/body';
+
+
+
+import footerComponent from '../components/footer';
+
+
+const ERROR_BODY = '`bodyContent` is a required option for `renderTemplate`';
+
+const checkingBodyContent = (bodyContent) => {
+    if (!bodyContent) {
+        throw new Error(ERROR_BODY);
+    }
+}
+
+let logoTop = '';
+let logoBottom = '';
+let content = '';
+
+// note that footer param here is a subcomponent, 
+// so we passing footerDisplay instead of just a component
+const DisplayBody = {
+  component: EmailTemplateBodyComponent,
+  params: { footerComponent, logoTop, logoBottom, content },
+  display: () => {
+      
+      //console.log(this.component(params))
+      
+      
+  },
+  checks: () => [
+  ]
+  
+}
+
+export default DisplayBody;
