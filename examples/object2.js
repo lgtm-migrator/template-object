3ffff5ea22aec61c2b597288235cd10f9b314e32
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-06-19 22:49:17 +0300

    Update object2.js

--- examples/object2.js
+++ examples/object2.js
@@ -5,21 +5,7 @@ const ConverterBody = {
     value: header,
     //     title: "xxx",
   },
-  comments: {
-    key: 'REGEXP_HTML_COMMENTS',
-    //     value: 1,
-    //     title: "xxx",
-  },
-  previewText: {
-    key: 'REGEXP_PREVIEW_TEXT',
-    //     value: 1,
-    //     title: "xxx",
-  },
-  sponsor: {
-    key: 'REGEXP_SPONSORSHIP',
-    value: sponsorship,
-    //     title: "xxx",
-  },
+
 };
 
 module.exports = ConverterBody;
