f2d08039ae392ecbcb7ed04bcad0107d5d318603
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-06-21 21:51:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import FruitFactory from "./easyFactory";
 import displayFactoryTwo from "./factoryTwo";
 
+ const fruit = FruitFactory.create({size: 10, sugar: 10}, 'constructor argument');
+ console.log(fruit.name());
+
+
+
 export default {
     FruitFactory,
     displayFactoryTwo
